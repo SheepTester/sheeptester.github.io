03683cfafd48fd704be3d23326446fdf8dc63814
Sean <ussa.say@gmail.com>
2016-09-13 19:35:37 -0700

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -2,8 +2,9 @@
 document.body.innerHTML+='<div id="white"><img src="https://sheeptester.github.io/SHEEP.svg" id="sheepmenu"></div>';
 document.head.innerHTML+='<link rel="stylesheet" type="text/css" href="https://sheeptester.github.io/sheep.css">';
 document.querySelector("#sheepmenu").onclick=function(){
-  document.querySelector("#sheepmenu").className="sheepmovingtime";
+  document.querySelector("#sheepmenu").className="sheepmenu";
+  document.querySelector("#sheepmenu").id="";
   window.setTimeout(function(){
     window.location="https://sheeptester.github.io/";
-  },500);
+  },300);
 }
