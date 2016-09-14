365dc2ef483217a39413df6360cdabc684fdc386
Sean <ussa.say@gmail.com>
2016-09-13 18:25:44 -0700

    Create sheep.js

---
+++ sheep.js
@@ -0,0 +1,11 @@
+/* reference me and all of your worries about making everything perfect goes away! ;) */
+document.body.innerHTML+='<div id="header"><img src="file:///C:/Users/seant/OneDrive/Pictures/Sheep_maker.png" id="menu" style="box-shadow: rgb(136, 136, 136) 0px 0px 10px;"><ul id="mn" style="left: -157px;"><li><a href="#">Test</a></li><li><a href="#">Test</a></li></ul></div>';
+/* menu
+<div id="header">
+	<img src="file:///C:/Users/seant/OneDrive/Pictures/Sheep_maker.png" id="menu" style="box-shadow: rgb(136, 136, 136) 0px 0px 10px;">
+	<ul id="mn" style="left: -157px;">
+		<li><a href="#">Test</a></li>
+		<li><a href="#">Test</a></li>
+	</ul>
+</div>
+*/
