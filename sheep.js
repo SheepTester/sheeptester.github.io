4eee4e8fe26e01335ee94a0f056b3fdd36f4127e
Sean <ussa.say@gmail.com>
2016-09-13 18:47:33 -0700

    saving

--- sheep.js
+++ sheep.js
@@ -1,6 +1,7 @@
 /* reference me and all of your worries about making everything perfect goes away! ;) */
-document.body.innerHTML+='<div id="sheepmenu"><img src="Sheep_maker.png" id="menu" style="box-shadow: rgb(136, 136, 136) 0px 0px 10px;"></div>';
-document.head.innerHTML+='<link rel="stylesheet" type="text/css" href="sheep.css">';
-/* menu
-<ul id="mn" style="left: -157px;"><li><a href="#">Test</a></li><li><a href="#">Test</a></li></ul>
-*/
+document.body.innerHTML+='<div id="white"><img src="Sheep_maker.png" id="sheepmenu"></div>';
+document.head.innerHTML+='<link rel="stylesheet" type="text/css" href="https://sheeptester.github.io/sheep.css">';
+document.querySelector("#sheepmenu").onclick=function(){
+  document.querySelector("#sheepmenu").className="sheepmovingtime";
+  document.querySelector("#sheepmenu").style.animation="wow 1s";
+}
