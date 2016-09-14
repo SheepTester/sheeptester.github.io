0285a72cc194a2682e2eeef0ffaec28d28656341
Sean <ussa.say@gmail.com>
2016-09-13 19:28:22 -0700

    okie dokers, fixed images on other pages meh meh
    meh

--- sheep.js
+++ sheep.js
@@ -1,9 +1,9 @@
 /* reference me and all of your worries about making everything perfect goes away! ;) */
-document.body.innerHTML+='<div id="white"><img src="SHEEP.svg" id="sheepmenu"></div>';
+document.body.innerHTML+='<div id="white"><img src="https://sheeptester.github.io/SHEEP.svg" id="sheepmenu"></div>';
 document.head.innerHTML+='<link rel="stylesheet" type="text/css" href="https://sheeptester.github.io/sheep.css">';
 document.querySelector("#sheepmenu").onclick=function(){
   document.querySelector("#sheepmenu").className="sheepmovingtime";
   window.setTimeout(function(){
     window.location="https://sheeptester.github.io/";
-  },1000);
+  },500);
 }
