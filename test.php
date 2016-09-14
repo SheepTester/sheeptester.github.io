cceecfd3c3b62811276566803d19c5b24f9fc7ae
Sean <ussa.say@gmail.com>
2016-09-13 17:55:53 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
-<php?
 
+<html>
+  <body>unhappy face<php?
 for ($x = 0; $x <= 100; $x++) {
   mail("luminlu@yahoo.com","more time","lost saved files need more time");
+  echo "hi";
 } 
-?>
-<html>
-  <body>unhappy face</body>
+?></body>
 </html>
