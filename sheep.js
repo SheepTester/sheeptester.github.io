ef473bc2863555eb3b30319a39d94ff085dd0e06
Sean <ussa.say@gmail.com>
2016-09-13 18:29:49 -0700

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -1,5 +1,5 @@
 /* reference me and all of your worries about making everything perfect goes away! ;) */
-document.body.innerHTML+='<div id="sheepmenu"><img src="SHEEP.png" id="menu" style="box-shadow: rgb(136, 136, 136) 0px 0px 10px;"></div>';
+document.body.innerHTML+='<div id="sheepmenu"><img src="Sheep_maker.png" id="menu" style="box-shadow: rgb(136, 136, 136) 0px 0px 10px;"></div>';
 document.head.innerHTML+='<link rel="stylesheet" type="text/css" href="sheep.css">';
 /* menu
 <ul id="mn" style="left: -157px;"><li><a href="#">Test</a></li><li><a href="#">Test</a></li></ul>
