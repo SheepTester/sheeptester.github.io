0666058cf517ebee10272cac2365a6565634390e
Sean <ussa.say@gmail.com>
2016-09-15 20:59:10 -0700

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,3 +1,9 @@
 setTimeout(function(){
   document.title="WHY ARE YOU STILL HERE?!";
 }, 60000);
+/*setInterval(function(){
+  document.querySelector(".rain").innerHTML+="<li></li>";
+  setTimeout(function(){
+    document.querySelector(".rain").removeChild()
+  }, 500);
+},100);*/
