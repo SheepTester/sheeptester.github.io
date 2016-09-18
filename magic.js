d73efe1889a99a6174a740b056e40df3908e56a7
Sean <ussa.say@gmail.com>
2016-09-17 20:32:45 -0700

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,8 +1,7 @@
 setTimeout(function(){
   document.title="WHY ARE YOU STILL HERE?!";
 }, 60000);
-var id=0,idn=0;
-setInterval(function(){
+var id=0,idn=0,rain=setInterval(function(){
   document.querySelector(".rain").innerHTML+="<li id='b"+id+"' style='left:"+Math.floor(Math.random()*window.innerWidth)+"px;'></li>";
   id++;
   setTimeout(function(){
@@ -10,3 +9,4 @@ setInterval(function(){
     idn++;
   }, 500);
 },50);
+/* clearInterval(rain); */
