784e8ee0d2eb5e6bace0f7701ca4c6d4e1f4a5b3
Sean <ussa.say@gmail.com>
2016-10-28 20:27:53 -0700

    Redesigned index page

--- magic.js
+++ magic.js
@@ -1,7 +1,7 @@
 setTimeout(function(){
   document.title="WHY ARE YOU STILL HERE?!";
 }, 60000);
-var ids=0,idn=0,rain=setInterval(function(){
+/*var ids=0,idn=0,rain=setInterval(function(){
   var s=document.createElement("LI");
   s.id="b"+ids;
   s.style.left=Math.floor(Math.random()*window.innerWidth)+"px";
@@ -11,14 +11,23 @@ var ids=0,idn=0,rain=setInterval(function(){
     document.querySelector(".rain").removeChild(document.querySelector("#b"+idn));
     idn++;
   }, 300);
-},50);
+},50);*/
 //clearInterval(rain);
+window.onload=function() {
+  if (window.location.hash) {
+    document.querySelector(".active").className="";
+    document.querySelector(".pgactive").className="";
+    document.querySelector("#btn"+window.location.hash.slice(1)).className="active";
+    document.querySelector("#pg"+window.location.hash.slice(1)).className="pgactive";
+  }
+};
 document.querySelector("#menu").onclick=function(e){
   if (e.target.tagName=="LI") {
     document.querySelector(".active").className="";
     document.querySelector(".pgactive").className="";
     e.target.className="active";
     document.querySelector("#pg"+e.target.id.slice(3)).className="pgactive";
+    window.location.hash=e.target.id.slice(3);
   }
 }
 document.querySelector("#pgContact ul").onclick=function(e){
