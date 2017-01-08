02b61c56f0bccbbf9e26cbaee07715d7c18275a1
Sean <ussa.say@gmail.com>
2017-01-07 18:30:27 -0800

    method 4!

--- sentence/flagyn.js
+++ sentence/flagyn.js
@@ -13,7 +13,7 @@ var methods={
         ['j','',''],
         ['k','nmc','hlr'],
         ['l','rpkw','p'],
-        ['m','lr','ntrszp'],
+        ['m','lr','ntrszpb'],
         ['n','mh','tp'],
         ['p','lrm','lrsh'],
         ['ph','lrm',''],
@@ -66,6 +66,43 @@ var methods={
     for (var i=0;i<length;i++) {
       if (onvowel) word+=vowels[Math.floor(Math.random()*vowels.length)];
       else word+=consonants[Math.floor(Math.random()*consonants.length)];
+      onvowel=!onvowel;
+    }
+    return word;
+  },
+  easypron:function(){
+    var data={
+      consonants:[ // ['letter','addafter'] bcdfghjk(l)mnpq(r)(s)tvwx(z) NOTE: What about doubles like 'eddie'?
+        ['b','rl'],
+        ['c','krl'],
+        ['ch','l'],
+        ['d','r'],
+        ['f','lr'],
+        ['g','nlr'],
+        'h','j','k',
+        ['l','dfmptv'],
+        ['m','npbt'],
+        ['n','t.th'.split('.')],
+        ['p','rlh'],
+        ['ph'],['qu'],
+        ['r',['st']],
+        ['s','khtml'],
+        ['t','lr'],
+        ['th','r'],
+        ['v','rl'],
+        ['w','h'],
+        'x','z'
+      ],
+      vowels:'a.e.i.o.u.y.ai.ou.oo.ee'.split('.')
+    },word='',length=Math.floor(Math.random()*5+2),onvowel=!Math.floor(Math.random()*2);
+    for (var i=0;i<length;i++) {
+      if (onvowel) word+=data.vowels[Math.floor(Math.random()*data.vowels.length)];
+      else {
+        var j=Math.floor(Math.random()*data.consonants.length);
+        word+=data.consonants[j][0];
+        if (data.consonants[j][2]&&i<length-1&&!Math.floor(Math.random()*3))
+          word+=data.consonants[j][2][Math.floor(Math.random()*data.consonants[j][2].length)];
+      }
       onvowel=!onvowel;
     }
     return word;
