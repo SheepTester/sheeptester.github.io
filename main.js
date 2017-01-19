66f1ce85fb952d986f62f357526598890932be29
Sean <ussa.say@gmail.com>
2017-01-18 17:42:44 -0800

    Faster redirects, #1 weekly, sheep anime

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 var sheeptest=(function(){
-  var redirects={
-    "platformre":"https://sheeptester.github.io/platformre/",
-    "makelevel":"https://sheeptester.github.io/platformre/maker/",
-    "gamepro5":"https://www.youtube.com/channel/UCFXJec3TUakUam-Xgll2-Ow",
-    "panthre":"https://www.youtube.com/channel/UCS5jP47FySoyh7tXYHDwQoA",
-    "penland":"https://sheeptester.github.io/platformre/penland/inv.html",
-    "hotel":"http://176.32.230.44/thingkingland.com/thehotel/",
-    "spam":"http://176.32.230.44/thingkingland.com/spammer/",
-    "dairy":"http://176.32.230.44/thingkingland.com/mysql/dairy.php",
-    'gaffasaurus':'https://www.youtube.com/channel/UCrvlbaPVXWjHGG3YtelpbNw/',
-  };
+  if (document.querySelector('sheepmenu')) {
+    document.querySelector('sheepmenu').classList.add('blockvision');
+    document.body.onload=_=>{
+      document.querySelector('sheepmenu').classList.remove('blockvision');
+      document.querySelector('sheepmenu').classList.add('playhellos');
+      setTimeout(_=>document.querySelector('sheepmenu').classList.remove('playhellos'),300);
+    };
+  }
   if (window.location.search) {
     var redirect=window.location.search.slice(1);
-    if (redirects.hasOwnProperty(redirect)) window.location.replace(redirects[redirect]);
-    else if (redirect.slice(0,2)=="rr") window.location.replace("https://i.cloudup.com/LE0xyc4dgp.mp4");
-    else if (redirect.slice(0,2)=="js") window.location.replace("https://akk.li/pics/anne.jpg");
-    else if (redirect.slice(0,3)=="try") window.location.replace("https://www.google.com/#tbm=isch&q=trypophobia");
-    else if (redirect.slice(0,11)=="javascript:") console.log('%ceval is evil','color:red;font-size:100px;');
-    else if (redirect.slice(0,8)=="existing") {
+    if (redirect.slice(0,8)=="existing") {
       window.location.hash='users';
       document.querySelector("#createform .name").value=redirect.slice(9);
       document.querySelector("#createform .error:first-of-type").innerHTML='User already exists.';
---
+++ redirects.js
@@ -0,0 +1,21 @@
+(function(){
+  var redirects={
+    "platformre":"https://sheeptester.github.io/platformre/",
+    "makelevel":"https://sheeptester.github.io/platformre/maker/",
+    "gamepro5":"https://www.youtube.com/channel/UCFXJec3TUakUam-Xgll2-Ow",
+    "panthre":"https://www.youtube.com/channel/UCS5jP47FySoyh7tXYHDwQoA",
+    "penland":"https://sheeptester.github.io/platformre/penland/inv.html",
+    "hotel":"http://176.32.230.44/thingkingland.com/thehotel/",
+    "spam":"http://176.32.230.44/thingkingland.com/spammer/",
+    "dairy":"http://176.32.230.44/thingkingland.com/mysql/dairy.php",
+    'gaffasaurus':'https://www.youtube.com/channel/UCrvlbaPVXWjHGG3YtelpbNw/',
+  };
+  if (window.location.search) {
+    var redirect=window.location.search.slice(1);
+    if (redirects.hasOwnProperty(redirect)) window.location.replace(redirects[redirect]);
+    else if (redirect.slice(0,2)=="rr") window.location.replace("https://i.cloudup.com/LE0xyc4dgp.mp4");
+    else if (redirect.slice(0,2)=="js") window.location.replace("https://akk.li/pics/anne.jpg");
+    else if (redirect.slice(0,3)=="try") window.location.replace("https://www.google.com/#tbm=isch&q=trypophobia");
+    else if (redirect.slice(0,11)=="javascript:") console.log('%ceval is evil','color:red;font-size:100px;');
+  }
+})();
