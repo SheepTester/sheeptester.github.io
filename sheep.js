9a1e0f3b74b608cd624986f3bb18f2d495156276
Sean <ussa.say@gmail.com>
2017-01-23 18:43:46 -0800

    Sean puns

--- sheep.js
+++ sheep.js
@@ -119,6 +119,14 @@ var SHEEP={
       return result;
     }
     else return undefined;
+  },
+  ajax(url,callback) {
+    var xmlHttp=new XMLHttpRequest();
+    xmlHttp.onreadystatechange=function(){
+      if (xmlHttp.readyState===4&&xmlHttp.status===200) callback(xmlHttp.responseText);
+    };
+    xmlHttp.open("GET",url,true); // true for asynchronous
+    xmlHttp.send(null);
   }
 };
 (function(){
