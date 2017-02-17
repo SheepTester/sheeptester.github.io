85458771159001b29b662714a61467f8754569ef
Sean <ussa.say@gmail.com>
2017-02-16 19:52:14 -0800

    Unshipped people don't have a blank spot anymore

--- stalk/main.js
+++ stalk/main.js
@@ -37,22 +37,22 @@
       document.querySelector('#race').innerHTML=data.race||'unknown';
       document.querySelector('#bday').innerHTML=data.birthday?data.birthday[0]+' '+months[data.birthday[1]]+' '+data.birthday[2]:'unknown';
       document.querySelector('#born').innerHTML=data.born||'unknown';
-      document.querySelector('#alias').innerHTML=data.aliases.join(', ');
+      document.querySelector('#alias').innerHTML=data.aliases.join(', ')||"none";
       s='';
       for (var i=0;i<data.ships.length;i+=2) s+=data.ships[i]+' ('+data.ships[i+1]+'), ';
-      document.querySelector('#ship').innerHTML=s.slice(0,-2);
+      document.querySelector('#ship').innerHTML=s.slice(0,-2)||"none";
       s='';
       for (var i=0;i<data.schools.length;i+=3) {
         s+=data.schools[i];
         if (data.schools[i+1]===data.schools[i+2]) s+=' ('+data.schools[i+1]+'), ';
         else s+=' ('+data.schools[i+1]+'-'+data.schools[i+2]+'), ';
       }
-      document.querySelector('#edu').innerHTML=s.slice(0,-2);
-      document.querySelector('#org').innerHTML=data.organisations.join(', ');
+      document.querySelector('#edu').innerHTML=s.slice(0,-2)||"unknown";
+      document.querySelector('#org').innerHTML=data.organisations.join(', ')||"none";
       s='';
       for (var i=0;i<data.profiles.length;i+=2) s+='<a href="'+data.profiles[i+1]+'">'+data.profiles[i]+'</a> ';
-      document.querySelector('#profs').innerHTML=s.slice(0,-1);
-      document.querySelector('#desc').innerHTML=data.desc;
+      document.querySelector('#profs').innerHTML=s.slice(0,-1)||"none";
+      document.querySelector('#desc').innerHTML=data.desc||"is a human I think";
       // --- END ---
       document.body.classList.add('bio');
       document.body.addEventListener("mousedown",leave,false);
