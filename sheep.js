b801b8fc857ea030a39afa13256e3a8fb0591550
Sean <ussa.say@gmail.com>
2017-03-11 20:04:51 -0800

    no more blurry canvases!

--- sheep.js
+++ sheep.js
@@ -227,6 +227,17 @@ var SHEEP={
       elem.addEventListener("mousedown",e=>mousedown(false,e),false);
       elem.addEventListener("touchstart",e=>mousedown(true,e.touches[0]),false);
     }
+  },
+  pixelratio() {
+    // http://stackoverflow.com/questions/15661339/how-do-i-fix-blurry-text-in-my-html5-canvas
+    var ctx=document.createElement("canvas").getContext("2d"),
+    dpr=window.devicePixelRatio||1,
+    bsr=ctx.webkitBackingStorePixelRatio||
+      ctx.mozBackingStorePixelRatio||
+      ctx.msBackingStorePixelRatio||
+      ctx.oBackingStorePixelRatio||
+      ctx.backingStorePixelRatio||1;
+    return dpr/bsr;
   }
 };
 (function(){
