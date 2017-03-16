33f9e1e452ab9a6b576092ac1c359c90675dd111
Sean <ussa.say@gmail.com>
2017-03-15 20:59:16 -0700

    one two three wood head person

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,10 +1,10 @@
-var events='focus blur click resize scroll cut copy paste keydown keyup keypress mouseenter mouseover mousemove mousedown mouseup dblclick contextmenu wheel mouseleave mouseout select dragstart drag dragend dragenter dragover dragleave drop fullscreenchange beforeprint afterprint pagehide pageshow popstate cached unload beforeunload load online offline open close'.split(' '),
+var events='focus blur click resize scroll cut copy paste keydown keyup keypress mouseenter mouseover mousemove mousedown mouseup dblclick contextmenu wheel mouseleave mouseout select dragstart drag dragend dragenter dragover dragleave drop fullscreenchange beforeprint afterprint pagehide pageshow popstate cached unload beforeunload load online offline open close touchstart touchmove touchend'.split(' '),
 statinner='<button id="hide">Hide stats</button><br>';
 for (var i=0;i<events.length;i++) {
   statinner+=events[i]+': <span id="stat'+events[i]+'">0</span><br>'
   document.addEventListener(events[i],(e)=>{
     document.querySelector('#stat'+e.type).innerHTML=Number(document.querySelector('#stat'+e.type).innerHTML)+1;
-    if (!'mousemove drag dragover'.split(' ').includes(e.type)) {
+    if (!'mousemove drag dragover touchmove'.split(' ').includes(e.type)) {
       var s=document.createElement('li');
       s.innerHTML=e.type;
       setTimeout(()=>{
