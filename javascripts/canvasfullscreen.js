2a97b89156360a5abeede43d7dd050aaf60a6462
Sean <ussa.say@gmail.com>
2017-11-06 20:40:06 -0800

    Trump

--- javascripts/canvasfullscreen.js
+++ javascripts/canvasfullscreen.js
@@ -1,15 +1,18 @@
 window.addEventListener("DOMContentLoaded",e=>{
-  var canvas=document.querySelector('body > canvas'),
-  c=canvas.getContext('2d');
   function resize() {
-    var pixelratio=(window.devicePixelRatio||1)/(c.webkitBackingStorePixelRatio||c.mozBackingStorePixelRatio||c.msBackingStorePixelRatio||c.oBackingStorePixelRatio||c.backingStorePixelRatio||1);
-    canvas.width=pixelratio*window.innerWidth;
-    canvas.height=pixelratio*window.innerHeight;
-    c.scale(pixelratio,pixelratio);
-    if (canvas.classList.contains('pixelated')) {
-      c.imageSmoothingEnabled=false;
-      c.mozImageSmoothingEnabled=false;
-      c.webkitImageSmoothingEnabled=false;
+    var canvases=document.querySelectorAll('body>canvas');
+    for (var i=0;i<canvases.length;i++) {
+      var canvas=canvases[i],
+      c=canvas.getContext('2d'),
+      pixelratio=(window.devicePixelRatio||1)/(c.webkitBackingStorePixelRatio||c.mozBackingStorePixelRatio||c.msBackingStorePixelRatio||c.oBackingStorePixelRatio||c.backingStorePixelRatio||1);
+      canvas.width=pixelratio*window.innerWidth;
+      canvas.height=pixelratio*window.innerHeight;
+      c.scale(pixelratio,pixelratio);
+      if (canvas.classList.contains('pixelated')) {
+        c.imageSmoothingEnabled=false;
+        c.mozImageSmoothingEnabled=false;
+        c.webkitImageSmoothingEnabled=false;
+      }
     }
   }
   window.addEventListener("resize",resize,false);
