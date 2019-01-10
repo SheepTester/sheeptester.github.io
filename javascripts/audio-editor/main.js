a28e46caec0225a5c56e5b09fe4cc1db0c85b9fa
SheepTester <ussa.say@gmail.com>
2019-01-09 19:25:35 -0800

    recording

--- javascripts/audio-editor/main.js
+++ javascripts/audio-editor/main.js
@@ -5,6 +5,8 @@ import SharedAudioContext from './audio/shared-audio-context.js';
 
 import SoundEditor from './sound-editor.js';
 
+import AudioRecorder from './audio/audio-recorder.js';
+
 const adjectives = [
   'new',
   'pretty cool',
@@ -20,9 +22,9 @@ const adjectives = [
   'okay',
   'audible'
 ];
-function randomName() {
+function randomName(noun) {
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  return `A${'aeiou'.includes(adjective[0]) ? 'n' : ''} ${adjective} sound`;
+  return `A${'aeiou'.includes(adjective[0]) ? 'n' : ''} ${adjective} ${noun}`;
 }
 
 document.addEventListener('DOMContentLoaded', e => {
@@ -50,11 +52,49 @@ document.addEventListener('DOMContentLoaded', e => {
     const editor = new SoundEditor({
       sampleRate: 48000,
       samples: new Float32Array([0]),
-      name: randomName(),
+      name: randomName('sound'),
       clipboard: clipboard
     });
     document.body.appendChild(createElement('hr'))
     document.body.appendChild(editor.render());
     editor.renderWaveform();
+  });
+  const recordSound = document.getElementById('record-sound');
+  const stopRecord = document.getElementById('stop-record');
+  const recordingDisplay = document.getElementById('record-display');
+  recordingDisplay.style.display = 'none';
+  let audioRecorder = null;
+  function levelUpdate(level) {
+    recordingDisplay.style.setProperty('--level', level * 100 + '%');
+  }
+  recordSound.addEventListener('click', e => {
+    if (audioRecorder) return;
+    recordSound.disabled = true;
+    stopRecord.disabled = false;
+    audioRecorder = new AudioRecorder();
+    audioRecorder.startListening(() => {
+      recordingDisplay.style.display = null;
+      audioRecorder.startRecording();
+    }, levelUpdate, () => {
+      alert('recording problem');
+    });
+  });
+  stopRecord.addEventListener('click', e => {
+    if (!audioRecorder) return;
+    recordSound.disabled = false;
+    stopRecord.disabled = true;
+    const {samples, sampleRate} = audioRecorder.stop();
+    const editor = new SoundEditor({
+      sampleRate: sampleRate,
+      samples: samples,
+      name: randomName('recording'),
+      clipboard: clipboard
+    });
+    document.body.appendChild(createElement('hr'))
+    document.body.appendChild(editor.render());
+    editor.renderWaveform();
+    audioRecorder.dispose();
+    audioRecorder = null;
+    recordingDisplay.style.display = 'none';
   });
 });
