06560e8e9be7f2b3462a3c0bc43f987d460d38f1
SheepTester <ussa.say@gmail.com>
2019-03-30 19:24:35 -0700

    minified images

---
+++ img/compress.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const execFile = require('child_process').execFile;
+const pngquant = require('pngquant-bin');
+
+function getFileSize(file) {
+  return new Promise((res, rej) => {
+    fs.stat(path.resolve(__dirname, file), (err, stats) => {
+      if (err) rej(err);
+      else res(stats.size);
+    });
+  });
+}
+
+const images = [
+  'all2.png',
+  'aplat.png',
+  'ugwa.png',
+  'colourpick.png',
+  'platmakre.png',
+  'newpenland.png',
+  'turkeys.png',
+  'tele.png',
+  'click.png',
+  'reds.png',
+  'color.png',
+  'connect4.png',
+  'gunn student simulator.png',
+  'fgr.png',
+  'js.png',
+  'misc.png',
+  'thingkingland.png',
+  'particles.png',
+  'minigames.png',
+  'helloworlddir.png',
+  'trumpdays.png',
+  'audio-editor.png',
+  'roots.png',
+  'mems.png',
+  'sieve.png',
+  'anima.png',
+  'sentence.png',
+  'rightclick.png',
+  'longtweets.png',
+  'billy-goat.png',
+  'carecalc.png',
+  'fword.png',
+  'mspaint.png',
+  'olamreee.png',
+  'scheme.png',
+  'cyrillic.png',
+  'terminal.png',
+  'penland.png',
+  'life.png',
+  'mars.png',
+  'ovinetopia.png',
+  'eyo.png',
+  'die.png',
+].map(img => path.resolve(__dirname, img));
+
+console.log('Starting...');
+execFile(pngquant, ['--quality=60-80', '--ext', '-smaller.png', '--force', ...images], err => {
+  if (err) console.log(err);
+  else console.log('Done. Comparing fat vs compressed...');
+  Promise.all(images.map(img => Promise.all([
+    getFileSize(img),
+    getFileSize(img.replace('.png', '-smaller.png'))
+  ]).then(([old, compressed]) =>
+      Math.round((old - compressed) / old * 100).toString().padStart(4, ' ') + '% ' + img)))
+    .then(reductions => {
+      console.log('Reductions:\n' + reductions.join('\n'));
+    });
+});
