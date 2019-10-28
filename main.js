804652af86a1ce63fae0c900f38c4efe17fae139
Sean <ussa.say@gmail.com>
2019-10-28 10:45:24 -0700

    show only certain tags in home page

--- main.js
+++ main.js
@@ -76,74 +76,98 @@ document.addEventListener('DOMContentLoaded', e => {
     window.location.reload();
   });
 
-  if (!document.body.classList.contains('grid-view')) return;
-  const tags = {
-    game: 'Game',
-    recommend: 'Commendable',
-    utility: 'Utility',
-    visual: 'Visually pleasing',
-    directory: 'Directory',
-    school: 'For school',
-    incomplete: 'Incomplete',
-    unintuitive: 'Unintuitive'
-  };
-  const closeAboutBtn = document.getElementById('close-about');
-  const titleElem = document.getElementById('title');
-  const descElem = document.getElementById('desc');
-  const openBtn = document.getElementById('open');
-  const defaultTitle = titleElem.textContent;
-  const defaultDesc = descElem.textContent;
-  let selected = null;
-
-  for (const link of document.getElementById('links').children) {
-    const image = document.createElement('img');
-    image.addEventListener('load', e => {
-      image.classList.add('loaded');
-    });
-    image.src = link.dataset.image;
-    link.appendChild(image);
-
-    const title = link.querySelector('.name').textContent;
-    const desc = link.querySelector('.desc').textContent;
-    link.addEventListener('click', e => {
-      if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey || e.which !== 1) return;
-      if (link.classList.contains('selected')) {
-        selected = null;
-        link.classList.remove('selected');
-        document.body.classList.remove('selected-link');
-        titleElem.textContent = defaultTitle;
-        descElem.textContent = defaultDesc;
-        openBtn.style.backgroundImage = null;
-        openBtn.href = '#!';
+  const tags = {};
+  const filter = document.createElement('style');
+  document.head.appendChild(filter);
+  let selectedTag = null;
+  for (const tag of document.querySelectorAll('span.tag')) {
+    const tagID = tag.querySelector('span').className;
+    const tagName = tag.textContent.trim();
+    tags[tagID] = tagName;
+    tag.addEventListener('click', e => {
+      if (tag === selectedTag) {
+        tag.classList.remove('selected');
+        filter.textContent = '';
+        selectedTag = null;
       } else {
-        if (selected) {
-          selected.classList.remove('selected');
+        if (selectedTag) {
+          selectedTag.classList.remove('selected');
         }
-        selected = link;
-        link.classList.add('selected');
-        document.body.classList.add('selected-link');
-        titleElem.textContent = title;
-        descElem.textContent = desc;
-        openBtn.style.backgroundImage = `radial-gradient(circle, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), url(${encodeURI(link.dataset.image)})`;
-        openBtn.href = link.href;
+        tag.classList.add('selected');
+        filter.textContent = `.list a:not(.is-${tagID}) { display: none; }`;
+        selectedTag = tag;
       }
-      e.preventDefault();
     });
+  }
+  document.body.classList.add('interactive-tags');
 
-    for (const tag of link.querySelector('.tags').children) {
-      tag.title = tags[tag.className];
+  if (document.body.classList.contains('grid-view')) {
+    const closeAboutBtn = document.getElementById('close-about');
+    const titleElem = document.getElementById('title');
+    const descElem = document.getElementById('desc');
+    const openBtn = document.getElementById('open');
+    const defaultTitle = titleElem.textContent;
+    const defaultDesc = descElem.textContent;
+    let selected = null;
+
+    for (const link of document.getElementById('links').children) {
+      const image = document.createElement('img');
+      image.addEventListener('load', e => {
+        image.classList.add('loaded');
+      });
+      image.src = link.dataset.image;
+      link.appendChild(image);
+
+      const title = link.querySelector('.name').textContent;
+      const desc = link.querySelector('.desc').textContent;
+      link.addEventListener('click', e => {
+        if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey || e.which !== 1) return;
+        if (link.classList.contains('selected')) {
+          selected = null;
+          link.classList.remove('selected');
+          document.body.classList.remove('selected-link');
+          titleElem.textContent = defaultTitle;
+          descElem.textContent = defaultDesc;
+          openBtn.style.backgroundImage = null;
+          openBtn.href = '#!';
+        } else {
+          if (selected) {
+            selected.classList.remove('selected');
+          }
+          selected = link;
+          link.classList.add('selected');
+          document.body.classList.add('selected-link');
+          titleElem.textContent = title;
+          descElem.textContent = desc;
+          openBtn.style.backgroundImage = `radial-gradient(circle, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), url(${encodeURI(link.dataset.image)})`;
+          openBtn.href = link.href;
+        }
+        e.preventDefault();
+      });
+
+      for (const tag of link.querySelector('.tags').children) {
+        tag.title = tags[tag.className];
+        link.classList.add('is-' + tag.className);
+      }
+    }
+
+    openBtn.addEventListener('click', e => {
+      if (!selected) {
+        document.body.classList.add('show-about');
+        closeAboutBtn.disabled = false;
+        e.preventDefault();
+      }
+    });
+    closeAboutBtn.addEventListener('click', e => {
+      document.body.classList.remove('show-about');
+      closeAboutBtn.disabled = true;
+    });
+  } else {
+    for (const link of document.getElementById('links').children) {
+      for (const tag of link.querySelector('.tags').children) {
+        tag.title = tags[tag.className];
+        link.classList.add('is-' + tag.className);
+      }
     }
   }
-
-  openBtn.addEventListener('click', e => {
-    if (!selected) {
-      document.body.classList.add('show-about');
-      closeAboutBtn.disabled = false;
-      e.preventDefault();
-    }
-  });
-  closeAboutBtn.addEventListener('click', e => {
-    document.body.classList.remove('show-about');
-    closeAboutBtn.disabled = true;
-  });
 });
