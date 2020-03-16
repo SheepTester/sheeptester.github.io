ad3f01fc442b575b508a324c9c431a5455b6c841
SheepTester <ussa.say@gmail.com>
2020-03-15 23:09:56 -0700

    WIP image translucentifier

---
+++ javascripts/accept-image.mjs
@@ -0,0 +1,70 @@
+export class AcceptImage {
+  constructor (onImage) {
+    this._onImages = []
+    if (onImage) {
+      if (typeof onImage !== 'function') {
+        throw new TypeError('`onImage` is not a function.')
+      }
+      this._onImages.push(onImage)
+    }
+
+    this._listening = false
+    this.image = null
+  }
+
+  onImage (fn) {
+    if (typeof fn !== 'function') {
+      throw new TypeError('Given value is not a function.')
+    }
+    this._onImages.push(fn)
+    return this
+  }
+
+  _callOnImages (...values) {
+    for (const onImage of this._onImages) {
+      onImage(...values)
+    }
+  }
+
+  _loadImage (file) {
+    if (file.type === 'image/svg+xml') {
+      return new Promise((resolve, reject) => {
+        const img = document.createElement('img')
+        img.addEventListener('load', e => {
+          resolve(img)
+        }, { once: true })
+        img.addEventListener('error', reject)
+        img.src = URL.createObjectURL(file)
+      })
+    } else {
+      return createImageBitmap(file)
+    }
+  }
+
+  async _acceptImage (file) {
+    this.image = file ? await this._loadImage(file) : null
+    this._callOnImages(this.image)
+  }
+
+  listen () {
+    if (this._listening) return
+    document.addEventListener('paste', e => {
+      this._acceptImage(e.clipboardData.files[0])
+    })
+    this._listening = true
+    return this
+  }
+
+  addFileInputTo (elem, labelText = '') {
+    const label = document.createElement('label')
+    label.className = 'file-input-label'
+    label.textContent = labelText
+    const input = document.createElement('input')
+    input.className = 'file-input'
+    input.type = 'file'
+    input.accept = 'image/*'
+    label.appendChild(input)
+    elem.appendChild(label)
+    return this
+  }
+}
---
+++ javascripts/image-transformer.mjs
@@ -0,0 +1,25 @@
+export class ImageTransformer {
+  constructor (transformFn) {
+    if (typeof transformFn !== 'function') {
+      throw new TypeError('`transformFn` is not a function.')
+    }
+    this._transformFn = transformFn
+
+    this.preview = document.createElement('canvas')
+    this._previewCtx = this.preview.getContext('2d')
+    this.output = document.createElement('canvas')
+    this._outputCtx = this.output.getContext('2d')
+  }
+
+  useImage (image) {
+    if (!image) return
+    this.preview.width = this.output.width = image.width
+    this.preview.height = this.output.height = image.height
+    this._previewCtx.drawImage(image, 0, 0)
+  }
+
+  update (...args) {
+    this._transformFn(this.output, this._outputCtx, ...args, this)
+    return this
+  }
+}
