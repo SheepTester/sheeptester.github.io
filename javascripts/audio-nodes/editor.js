84b125415e506b63fb37ca05bf0d579c9e8a0cf5
SheepTester <ussa.say@gmail.com>
2021-01-31 23:17:35 -0800

    Add test swoopy line

--- javascripts/audio-nodes/editor.js
+++ javascripts/audio-nodes/editor.js
@@ -1,11 +1,15 @@
 import { Node, audioNodeParams, audioNodeOptions } from './node.js'
 import { audioCtx } from './audio-context.js'
+import { SVG_NS } from './utils.js'
 
 export class Editor {
+  static SWOOPINESS = 100
+
   #palette
   #container
   #dragArea
   #dragging = 0
+  #connectionsPath
 
   constructor (palette, container) {
     this.#palette = palette
@@ -16,7 +20,28 @@ export class Editor {
     document.body.append(dragArea)
     this.#dragArea = dragArea
 
+    const path = document.createElementNS(SVG_NS, 'path')
+    this.#connectionsPath = path
+    // this.#setPath([[{ x: 100, y: 100 }, { x: 200, y: 300 }]])
+
+    const connections = document.createElementNS(SVG_NS, 'svg')
+    connections.className.baseVal = 'connections'
+    connections.append(path)
+    container.append(connections)
+
     palette.append(...this.#initPalette())
+  }
+
+  #setPath (connections) {
+    this.#connectionsPath.setAttributeNS(
+      null,
+      'd',
+      connections
+        .map(([{ x: x1, y: y1 }, { x: x2, y: y2 }]) => (
+          `M${x1} ${y1}C${x1 + Editor.SWOOPINESS} ${y1},${x2 - Editor.SWOOPINESS} ${y2},${x2} ${y2}`
+        ))
+        .join('')
+    )
   }
 
   #addNode (audioNode) {
