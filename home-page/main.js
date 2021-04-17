c4f2552976826790de4b35fd46c0c9ab88ee4eca
SheepTester <ussa.say@gmail.com>
2021-04-16 23:14:12 -0700

    Less fancy but IMO nicer descriptions

--- home-page/main.js
+++ home-page/main.js
@@ -6,6 +6,7 @@ for (const checkbox of document.getElementsByClassName('filter-checkbox')) {
   checkbox.addEventListener('change', e => {
     if (checkbox.checked) {
       selected.add(className)
+      removeDescription()
     } else {
       selected.delete(className)
     }
@@ -28,35 +29,39 @@ for (const label of document.getElementsByClassName('filter-tag')) {
   })
 }
 
-function createDescription () {
+function createDescription (from, link) {
   const div = document.createElement('div')
   div.className = 'description'
-  div.textContent = 'hello!'
+  for (const paragraph of from.split(/\r?\n/)) {
+    const p = document.createElement('p')
+    p.textContent = paragraph
+    div.append(p)
+  }
+  const a = document.createElement('a')
+  a.className = 'desc-visit block-link'
+  a.href = link
+  a.textContent = 'Visit'
+  div.append(a)
   return div
+}
+function removeDescription () {
+  if (lastSelected) {
+    const [project, description] = lastSelected
+    project.classList.remove('showing-desc')
+    description.remove()
+  }
 }
 const projectsWrapper = document.getElementById('projects-wrapper')
 let lastSelected = null
 projectsWrapper.addEventListener('click', e => {
-  if (e.detail === 0) {
-    // https://stackoverflow.com/a/55375571
-    // `detail` is 0 if the user clicked via the enter key (normally, it's the
-    // nth click in a series of click, like when double-clicking).
-    // However, this prevents them from seeing the description unless they have
-    // a screen reader, so I'm not sure if this is the best choice.
-    return
-  }
   const project = e.target.closest('.project')
   if (project) {
-    if (lastSelected) {
-      const [project, description] = lastSelected
-      project.classList.remove('showing-desc')
-      description.classList.add('desc-hide')
-      description.addEventListener('transitionend', e => {
-        description.remove()
-      })
-    }
+    removeDescription()
     if (!lastSelected || lastSelected[0] !== project) {
-      const description = createDescription()
+      const description = createDescription(
+        project.querySelector('.project-desc').textContent,
+        project.href
+      )
       lastSelected = [project, description]
       project.classList.add('showing-desc')
       project.after(description)
@@ -68,3 +73,7 @@ projectsWrapper.addEventListener('click', e => {
     }
   }
 })
+
+for (const project of document.getElementsByClassName('project')) {
+  project.ariaLabel = 'View details'
+}
