4c143d788b56234ce58dd3eeb06fbb77446fd93d
SheepTester <ussa.say@gmail.com>
2021-04-16 22:46:40 -0700

    Description pop up design, but not a fan

--- home-page/main.js
+++ home-page/main.js
@@ -18,3 +18,53 @@ for (const checkbox of document.getElementsByClassName('filter-checkbox')) {
     }
   })
 }
+
+for (const label of document.getElementsByClassName('filter-tag')) {
+  // Prevent selecting text when spam clicking button
+  // Why not CSS? This way, you can still select the text by other means, like
+  // with normal <button>s
+  label.addEventListener('mousedown', e => {
+    e.preventDefault()
+  })
+}
+
+function createDescription () {
+  const div = document.createElement('div')
+  div.className = 'description'
+  div.textContent = 'hello!'
+  return div
+}
+const projectsWrapper = document.getElementById('projects-wrapper')
+let lastSelected = null
+projectsWrapper.addEventListener('click', e => {
+  if (e.detail === 0) {
+    // https://stackoverflow.com/a/55375571
+    // `detail` is 0 if the user clicked via the enter key (normally, it's the
+    // nth click in a series of click, like when double-clicking).
+    // However, this prevents them from seeing the description unless they have
+    // a screen reader, so I'm not sure if this is the best choice.
+    return
+  }
+  const project = e.target.closest('.project')
+  if (project) {
+    if (lastSelected) {
+      const [project, description] = lastSelected
+      project.classList.remove('showing-desc')
+      description.classList.add('desc-hide')
+      description.addEventListener('transitionend', e => {
+        description.remove()
+      })
+    }
+    if (!lastSelected || lastSelected[0] !== project) {
+      const description = createDescription()
+      lastSelected = [project, description]
+      project.classList.add('showing-desc')
+      project.after(description)
+    } else {
+      lastSelected = null
+    }
+    if (!e.target.closest('.open-directly')) {
+      e.preventDefault()
+    }
+  }
+})
