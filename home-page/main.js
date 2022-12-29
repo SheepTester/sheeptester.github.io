ff6c3c203a43cef08f27d3401df584f5a00698b8
Sean <ussa.say@gmail.com>
2022-12-28 19:52:18 -0800

    résumé: Update GPA, remove position: relative from sections home-page: Add more data
    Removed position: relative because it might've broken Workday's ability to parse links

--- home-page/main.js
+++ home-page/main.js
@@ -14,9 +14,10 @@ for (const checkbox of document.getElementsByClassName('filter-checkbox')) {
     if (selected.size === 0) {
       filterStyle.textContent = ''
     } else {
-      filterStyle.textContent = `.project{display:none}${
-        Array.from(selected, className => '.' + className).join('')
-      }{display:block}`
+      filterStyle.textContent = `.project{display:none}${Array.from(
+        selected,
+        className => '.' + className
+      ).join('')}{display:block}`
     }
   })
 }
@@ -45,7 +46,9 @@ function createTag (tagElem) {
   const tag = document.createElement('div')
   tag.className = 'desc-tag'
   const icon = document.createElement('div')
-  icon.className = `desc-tag-icon base-tag ${[...tagElem.classList].find(cls => cls.startsWith('tag-'))}`
+  icon.className = `desc-tag-icon base-tag ${[...tagElem.classList].find(cls =>
+    cls.startsWith('tag-')
+  )}`
   const name = document.createElement('div')
   name.className = 'desc-tag-name'
   name.textContent = tagElem.title
@@ -100,7 +103,7 @@ for (const project of document.getElementsByClassName('project')) {
 
 function easeInOutCubic (t) {
   t *= 2
-  if (t < 1) return t * t * t / 2
+  if (t < 1) return (t * t * t) / 2
   t -= 2
   return (t * t * t + 2) / 2
 }
@@ -120,68 +123,74 @@ ageSpan.textContent = Math.floor((Date.now() - BIRTHDAY) / MS_IN_YR)
 ageSpan.classList.add('age-clickable')
 ageSpan.tabIndex = 0
 ageSpan.title = 'Click to see me age in real time'
-ageSpan.addEventListener('click', e => {
-  const ageWrapper = document.createElement('code')
-  ageWrapper.classList.add('age')
-  ageWrapper.role = 'text'
-  const age = getAge()
-  ageWrapper.style.width = age.length + 'ch'
-  const decimal = age.indexOf('.')
-  const digits = new Array(age.length)
-  let sigfigs = Math.floor((Date.now() - BIRTHDAY) / 10000).toString().length
-  for (let i = 0; i < age.length; i++) {
-    const digit = document.createElement('span')
-    digits[i] = {
-      elem: digit,
-      exponent: i === decimal
-        ? null
-        : i < decimal
-        ? decimal - i - 1
-        : decimal - i
-    }
-    if (age[i] !== '.') {
-      if (sigfigs <= 0) {
-        digit.classList.add('insignificant')
-        digit.title = 'This digit is purely an estimation.'
-      }
-      sigfigs--
-    } else {
-      digit.textContent = '.'
-      digit.style.transform = `translate3d(${i}ch, 0, 0)`
-    }
-    ageWrapper.append(digit)
-  }
-  ageWrapper.append('\xa0') // nbsp
-  ageSpan.replaceWith(ageWrapper)
-  function display () {
-    const now = Date.now()
-    const age = getAge(now)
+ageSpan.addEventListener(
+  'click',
+  e => {
+    const ageWrapper = document.createElement('code')
+    ageWrapper.classList.add('age')
+    ageWrapper.role = 'text'
+    const age = getAge()
+    ageWrapper.style.width = age.length + 'ch'
+    const decimal = age.indexOf('.')
+    const digits = new Array(age.length)
+    let sigfigs = Math.floor((Date.now() - BIRTHDAY) / 10000).toString().length
     for (let i = 0; i < age.length; i++) {
-      const digit = digits[i]
-      if (digit.exponent !== null) {
-        const interval = 10 ** digit.exponent * MS_IN_YR
-        const animationTime = Math.min(interval, ANIM_LENGTH)
-        const time = (now - BIRTHDAY) % interval
-        if (digit.elem.textContent !== age[i]) {
-          digit.elem.textContent = age[i]
+      const digit = document.createElement('span')
+      digits[i] = {
+        elem: digit,
+        exponent:
+          i === decimal ? null : i < decimal ? decimal - i - 1 : decimal - i
+      }
+      if (age[i] !== '.') {
+        if (sigfigs <= 0) {
+          digit.classList.add('insignificant')
+          digit.title = 'This digit is purely an estimation.'
         }
-        if (time < animationTime) {
-          const interp = easeInOutCubic(time / animationTime)
-          digit.elem.style.transform = `translate3d(${i}ch, ${interp - 1}em, 0)`
-          digit.elem.style.color = `rgba(255, 255, 255, ${interp * ALPHA})`
-          digit.elem.style.setProperty('--last', `rgba(255, 255, 255, ${(1 - interp) * ALPHA})`)
-          digit.elem.dataset.last = (+age[i] + 9) % 10
-          digit.wasStatic = false
-        } else if (!digit.wasStatic) {
-          digit.elem.style.transform = `translate3d(${i}ch, 0, 0)`
-          digit.elem.style.color = null
-          digit.elem.style.removeProperty('--last')
-          delete digit.elem.dataset.last
-          digit.wasStatic = true
+        sigfigs--
+      } else {
+        digit.textContent = '.'
+        digit.style.transform = `translate3d(${i}ch, 0, 0)`
+      }
+      ageWrapper.append(digit)
+    }
+    ageWrapper.append('\xa0') // nbsp
+    ageSpan.replaceWith(ageWrapper)
+    function display () {
+      const now = Date.now()
+      const age = getAge(now)
+      for (let i = 0; i < age.length; i++) {
+        const digit = digits[i]
+        if (digit.exponent !== null) {
+          const interval = 10 ** digit.exponent * MS_IN_YR
+          const animationTime = Math.min(interval, ANIM_LENGTH)
+          const time = (now - BIRTHDAY) % interval
+          if (digit.elem.textContent !== age[i]) {
+            digit.elem.textContent = age[i]
+          }
+          if (time < animationTime) {
+            const interp = easeInOutCubic(time / animationTime)
+            digit.elem.style.transform = `translate3d(${i}ch, ${
+              interp - 1
+            }em, 0)`
+            digit.elem.style.color = `rgba(255, 255, 255, ${interp * ALPHA})`
+            digit.elem.style.setProperty(
+              '--last',
+              `rgba(255, 255, 255, ${(1 - interp) * ALPHA})`
+            )
+            digit.elem.dataset.last = (+age[i] + 9) % 10
+            digit.wasStatic = false
+          } else if (!digit.wasStatic) {
+            digit.elem.style.transform = `translate3d(${i}ch, 0, 0)`
+            digit.elem.style.color = null
+            digit.elem.style.removeProperty('--last')
+            delete digit.elem.dataset.last
+            digit.wasStatic = true
+          }
         }
       }
+      window.requestAnimationFrame(display)
     }
-    window.requestAnimationFrame(display)
-  }
-  display()
-}, { once: true })
+    display()
+  },
+  { once: true }
+)
