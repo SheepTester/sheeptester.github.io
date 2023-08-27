ee29c95a3d285d92e8e18fe2ae971068c2143858
Sean <ussa.say@gmail.com>
2023-08-26 18:59:05 -0700

    Switch to a class-based API

--- javascripts/convolution-matrix-webgl.js
+++ javascripts/convolution-matrix-webgl.js
@@ -6,48 +6,100 @@
  */
 
 /**
- * @param {Context} gl
- * @param {Context['VERTEX_SHADER'] | Context['FRAGMENT_SHADER']} type
- * @param {string} source
- * @returns {WebGLShader}
+ * @typedef {object} ProgramShaders
+ * @property {string} vertex - Vertex shader source.
+ * @property {string} fragment - Fragment shader source.
  */
-export function createShader (gl, type, source) {
-  const shader = gl.createShader(type)
-  if (!shader) throw new TypeError("WebGL couldn't create shader.")
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.warn(gl.getShaderInfoLog(shader))
-    gl.deleteShader(shader)
-    throw new SyntaxError('Shader failed to compile')
+
+export class Program {
+  /** @type {Context} */
+  #gl
+  /** @type {WebGLProgram} */
+  #program
+
+  /**
+   * @param {Context} gl
+   * @param {ProgramShaders} shaders
+   */
+  constructor (gl, { vertex, fragment }) {
+    this.#gl = gl
+    this.#program = this.#createProgram([
+      this.#createShader(gl.VERTEX_SHADER, vertex),
+      this.#createShader(gl.VERTEX_SHADER, fragment)
+    ])
   }
-  return shader
+
+  /**
+   * @param {Context['VERTEX_SHADER'] | Context['FRAGMENT_SHADER']} type
+   * @param {string} source
+   * @returns {WebGLShader}
+   */
+  #createShader (type, source) {
+    const shader = this.#gl.createShader(type)
+    if (!shader) {
+      throw new TypeError("WebGL couldn't create shader.")
+    }
+    this.#gl.shaderSource(shader, source)
+    this.#gl.compileShader(shader)
+    if (!this.#gl.getShaderParameter(shader, this.#gl.COMPILE_STATUS)) {
+      console.warn(this.#gl.getShaderInfoLog(shader))
+      this.#gl.deleteShader(shader)
+      throw new SyntaxError('Shader failed to compile')
+    }
+    return shader
+  }
+
+  /**
+   * @param {WebGLShader[]} shaders
+   * @returns {WebGLProgram}
+   */
+  #createProgram (shaders) {
+    const program = this.#gl.createProgram()
+    if (!program) {
+      throw new TypeError("WebGL couldn't create program.")
+    }
+    for (const shader of shaders) {
+      this.#gl.attachShader(program, shader)
+    }
+    this.#gl.linkProgram(program)
+    if (!this.#gl.getProgramParameter(program, this.#gl.LINK_STATUS)) {
+      console.log(this.#gl.getProgramInfoLog(program))
+      this.#gl.deleteProgram(program)
+      throw new SyntaxError('Program failed to link shader.')
+    }
+    return program
+  }
+
+  /**
+   * @param {string} name - Name of attribute in shader source code.
+   */
+  attribute (name) {
+    const buffer = this.#gl.createBuffer()
+    if (!buffer) {
+      throw new TypeError("WebGL couldn't create buffer.")
+    }
+    return new Attribute(
+      this.#gl,
+      this.#gl.getAttribLocation(this.#program, name),
+      buffer
+    )
+  }
+
+  use () {
+    this.#gl.useProgram(this.#program)
+  }
 }
 
 /**
- * @param {Context} gl
- * @param {WebGLShader[]} shaders
- * @returns {WebGLProgram}
- */
-export function createProgram (gl, shaders) {
-  const program = gl.createProgram()
-  if (!program) throw new TypeError("WebGL couldn't create program.")
-  for (const shader of shaders) {
-    gl.attachShader(program, shader)
-  }
-  gl.linkProgram(program)
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.log(gl.getProgramInfoLog(program))
-    gl.deleteProgram(program)
-    throw new SyntaxError('Program failed to link shader.')
-  }
-  return program
-}
-
-/**
- * @typedef {object} Attribute
- * @property {number} location
- * @property {WebGLBuffer} buffer
+ * @typedef {object} AttributePointerOptions
+ * @property {number} components - Number of values per attribute value block.
+ * @property {GLenum} [type] - Type of each values. Defaults to `gl.FLOAT`
+ * (32-bit float).
+ * @property {boolean} [normalize] - Whether to normalize integers to floats.
+ * Doesn't do anything if `type` is `gl.FLOAT`. Defaults to false.
+ * @property {number} [spacing] - Number of bytes between attribute value
+ * blocks. Defaults to 0. (`stride`)
+ * @property {number} [start] - Start index in buffer. Defaults to 0. (`offset`)
  */
 
 /**
@@ -64,21 +116,76 @@ export function createProgram (gl, shaders) {
  *
  * If you do need to change the data, you can just call `gl.bufferData` during
  * render after binding the buffer to `ARRAY_BUFFER`.
- *
- * @param {Context} gl
- * @param {WebGLProgram} program
- * @param {string} attrName
- * @param {Float32Array} data
- * @returns {Attribute}
  */
-export function initAttrData (gl, program, attrName, data) {
-  const buffer = gl.createBuffer()
-  if (!buffer) throw new TypeError("WebGL couldn't create buffer.")
-  // Select the bind point `ARRAY_BUFFER`
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-  // `STATIC_DRAW` - Optimization hint meaning the data won't change often
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
-  return { location: gl.getAttribLocation(program, attrName), buffer }
+class Attribute {
+  /** @type {Context} */
+  #gl
+  /** @type {number} */
+  #location
+  /** @type {WebGLBuffer} */
+  #buffer
+
+  /**
+   * @param {Context} gl
+   * @param {number} location
+   * @param {WebGLBuffer} buffer
+   */
+  constructor (gl, location, buffer) {
+    this.#gl = gl
+    this.#location = location
+    this.#buffer = buffer
+  }
+
+  /**
+   * @returns {this}
+   */
+  enable () {
+    this.#gl.enableVertexAttribArray(this.#location)
+    return this
+  }
+
+  /**
+   *
+   */
+  bind () {
+    // Select the bind point `ARRAY_BUFFER`
+    this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffer)
+    return this
+  }
+
+  /**
+   * @param {BufferSource} data
+   * @returns {this}
+   */
+  data (data) {
+    // `STATIC_DRAW` - Optimization hint meaning the data won't change often
+    this.#gl.bufferData(this.#gl.ARRAY_BUFFER, data, this.#gl.STATIC_DRAW)
+    return this
+  }
+
+  /**
+   * @param {AttributePointerOptions} options
+   * @returns {this}
+   */
+  setPointer ({
+    components,
+    type = this.#gl.FLOAT,
+    normalize = false,
+    spacing = 0,
+    start = 0
+  }) {
+    // Implicitly binds the buffer bound to `ARRAY_BUFFER` directly to the
+    // attribute, so `ARRAY_BUFFER` can be bound to something else.
+    this.#gl.vertexAttribPointer(
+      this.#location,
+      components,
+      type,
+      normalize,
+      spacing,
+      start
+    )
+    return this
+  }
 }
 
 /**
@@ -86,16 +193,11 @@ export function initAttrData (gl, program, attrName, data) {
  */
 function test (gl) {
   // Init
-  const program = createProgram(gl, [
-    createShader(gl, gl.VERTEX_SHADER, ''),
-    createShader(gl, gl.FRAGMENT_SHADER, '')
-  ])
-  const position = initAttrData(
-    gl,
-    program,
-    'a_position',
-    new Float32Array([10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30])
-  )
+  const program = new Program(gl, { vertex: '', fragment: '' })
+  const position = program
+    .attribute('a_position')
+    .bind()
+    .data(new Float32Array([10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30]))
   const resolution = gl.getUniformLocation(program, 'u_resolution')
 
   // Render
@@ -106,19 +208,7 @@ function test (gl) {
 
   gl.useProgram(program)
 
-  gl.enableVertexAttribArray(position.location)
-  // Select position buffer
-  gl.bindBuffer(gl.ARRAY_BUFFER, position.buffer)
-  // Implicitly binds the buffer bound to `ARRAY_BUFFER` directly to the
-  // attribute, so `ARRAY_BUFFER` can be bound to something else. Parameters:
-  // - 2 components at a time; unspecified components of a vec4 default to 0, 0,
-  //   0, 1
-  // - buffer contains 32-bit floats
-  // - disable normalization (meant for normalizing ints to float)
-  // - 0 bytes of spacing between attribute value blocks
-  // - start at index 0 of buffer
-  gl.vertexAttribPointer(position.location, 2, gl.FLOAT, false, 0, 0)
-
+  position.enable().bind().setPointer({ components: 2 })
   gl.uniform2f(resolution, gl.canvas.width, gl.canvas.height)
 
   // Run vertex shader 6 times
