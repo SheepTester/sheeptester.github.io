7527d809f0df7bd505b8bbc81612991d1cbcccda
Sean <ussa.say@gmail.com>
2024-01-03 08:24:07 -0800

    reform: Redesign JPG quality
    would be nice to have the original file name though

--- reform/src/inputs.ts
+++ reform/src/inputs.ts
@@ -1,12 +1,14 @@
 import { Source } from './source'
 import { displayBytes } from './utils'
 
+const encoder = new TextEncoder()
+
 type FileInputOptions = {
   fileName?: Element | null
   input?: HTMLInputElement | null
   dropTarget?: HTMLElement | null
   pasteTarget?: boolean
-  onFile: (file: File) => Promise<void>
+  onFile: (file: File | string) => Promise<void>
 }
 function handleFileInput ({
   fileName = null,
@@ -15,32 +17,52 @@ function handleFileInput ({
   pasteTarget,
   onFile
 }: FileInputOptions): void {
-  async function handleFile (file?: File | null): Promise<void> {
-    if (file) {
+  async function handleFile (
+    items: FileList | DataTransfer | null
+  ): Promise<void> {
+    if (
+      items === null ||
+      (items instanceof FileList ? items : items.items).length === 0
+    ) {
+      return
+    }
+    const file =
+      items instanceof FileList
+        ? items[0]
+        : items.items[0].kind === 'string'
+        ? await new Promise<string>(resolve =>
+            items.items[0].getAsString(resolve)
+          )
+        : items.items[0].getAsFile()
+    if (file === null) {
+      return
+    }
+    if (fileName) {
+      fileName.textContent =
+        file instanceof File
+          ? `${file.name} · ${displayBytes(file.size)}`
+          : `Plain text · ${displayBytes(encoder.encode(file).length)}`
+      fileName.classList.remove('file-error')
+    }
+    try {
+      await onFile(file)
+    } catch (error) {
+      console.error(error)
       if (fileName) {
-        fileName.textContent = `${file.name} · ${displayBytes(file.size)}`
-        fileName.classList.remove('file-error')
-      }
-      try {
-        await onFile(file)
-      } catch (error) {
-        console.error(error)
-        if (fileName) {
-          fileName.textContent += ' — failed to load'
-          fileName.classList.add('file-error')
-        }
+        fileName.textContent += ' — failed to load'
+        fileName.classList.add('file-error')
       }
     }
   }
 
   input?.addEventListener('change', () => {
-    handleFile(input.files?.[0])
+    handleFile(input.files)
     // Reset files array (so can resubmit the same file multiple times)
     input.value = ''
   })
 
-  dropTarget?.addEventListener('drop', e => {
-    handleFile(e.dataTransfer?.files[0])
+  dropTarget?.addEventListener('drop', async e => {
+    handleFile(e.dataTransfer)
     dropTarget.classList.remove('drag-over')
     e.preventDefault()
   })
@@ -60,7 +82,7 @@ function handleFileInput ({
       ) {
         return
       }
-      handleFile(e.clipboardData?.files[0])
+      handleFile(e.clipboardData)
     })
   }
 }
@@ -69,14 +91,13 @@ export function handleImageInput (
   source: Source<HTMLCanvasElement>,
   input?: Element | null
 ): void {
-  const dropTarget = input?.closest('.two-col-io')
   if (input && !(input instanceof HTMLInputElement)) {
     console.warn(input, 'is not an <input> element')
     input = null
-  }
-  if (input) {
+  } else if (input) {
     input.dataset.ignore = 'true'
   }
+  const dropTarget = input?.closest('.reform\\:io')
   let maybeCanvas = dropTarget?.querySelector('.input-content canvas')
   if (maybeCanvas && !(maybeCanvas instanceof HTMLCanvasElement)) {
     console.warn(maybeCanvas, 'is not a <canvas> element')
@@ -90,6 +111,9 @@ export function handleImageInput (
     dropTarget: dropTarget instanceof HTMLElement ? dropTarget : undefined,
     pasteTarget: input?.classList.contains('reform:paste-target'),
     onFile: async file => {
+      if (typeof file === 'string') {
+        return
+      }
       const url = URL.createObjectURL(file)
       try {
         const image = document.createElement('img')
@@ -113,22 +137,22 @@ export function handleTextInput (
   source: Source<string>,
   input?: Element | null
 ): void {
-  const dropTarget = input?.closest('.two-col-io')
+  const wrapper = input?.closest('.reform\\:io')
   if (input && !(input instanceof HTMLInputElement)) {
     console.warn(input, 'is not an <input> element')
     input = null
-  }
-  if (input) {
+  } else if (input) {
     input.dataset.ignore = 'true'
   }
-  const textarea = dropTarget?.querySelector('.input-content')
+  const dropTarget = wrapper?.querySelector('.input-controls')
+  const textarea = wrapper?.querySelector('.input-content')
   handleFileInput({
     fileName: input?.parentElement?.querySelector('.file-name'),
     input,
     dropTarget: dropTarget instanceof HTMLElement ? dropTarget : undefined,
     pasteTarget: input?.classList.contains('reform:paste-target'),
     onFile: async file => {
-      const text = await file.text()
+      const text = file instanceof File ? await file.text() : file
       if (textarea instanceof HTMLTextAreaElement) {
         textarea.value = text
       }
