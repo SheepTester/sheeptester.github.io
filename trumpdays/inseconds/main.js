8646d349aea9bce2b9abd8733d12e57442f9b6a1
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2024-11-05 19:18:12 -0800

    in case trump wins i'll just have the web page set up
    hopefully i'm wrong! i'd rather undo this commit then see trump actually win

--- trumpdays/inseconds/main.js
+++ trumpdays/inseconds/main.js
@@ -1,14 +1,21 @@
 (function(inSeconds) {
-  var endDate=Number(new Date('2021-01-20T12:00:00-05:00'));
+  var endDate=Number(new Date('2025-01-20T12:00:00-05:00'));
+  if (new Date()>=endDate) {
+    endDate=Number(new Date('2029-01-20T12:00:00-05:00'));
+    document.querySelector('end').innerHTML="(it ends on the 20th of January, 2029)";
+    document.getElementById('until-end').innerHTML="until Trump's second (and hopefully final) term ends";
+  } else {
+    document.querySelector('end').innerHTML="(his inauguration is on the 20th of January, 2025)";
+    document.getElementById('until-end').innerHTML="until Trump is inaugurated";
+  }
   var divisor=inSeconds?1000:86400000;
-  document.querySelector('end').innerHTML="(it ends on the 20th of January, 2021)";
   var calc=_=>{
     var days=(endDate-Number(new Date()))/divisor;
     if (!inSeconds) days=Math.ceil(days);
     days=days.toString();
     if (days<=0) {
       document.querySelector('days').innerHTML="finally";
-      document.querySelector('sometext').innerHTML="Trump's first term is over!";
+      document.querySelector('sometext').innerHTML="Trump's second term is over!";
     } else if (days===1&&!inSeconds) {
       document.querySelector('days').innerHTML='Trump';
       document.querySelector('sometext').innerHTML='has one more day left';
