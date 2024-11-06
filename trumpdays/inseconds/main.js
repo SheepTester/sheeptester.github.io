058838d7ca0f22875e726f8a0a4a6bc7b9ac524b
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2024-11-05 19:07:57 -0800

    Polish trumpdays to make it easier to edit
    Predicting a trump victory https://twitter.com/Sheep_tester/status/1847329573232693565

--- trumpdays/inseconds/main.js
+++ trumpdays/inseconds/main.js
@@ -1,15 +1,24 @@
-(function() {
+(function(inSeconds) {
+  var endDate=Number(new Date('2021-01-20T12:00:00-05:00'));
+  var divisor=inSeconds?1000:86400000;
+  document.querySelector('end').innerHTML="(it ends on the 20th of January, 2021)";
   var calc=_=>{
-    var days=((Number(new Date('2021-01-20T12:00:00-05:00'))-Number(new Date()))/1000).toString();
+    var days=(endDate-Number(new Date()))/divisor;
+    if (!inSeconds) days=Math.ceil(days);
+    days=days.toString();
     if (days<=0) {
       document.querySelector('days').innerHTML="finally";
       document.querySelector('sometext').innerHTML="Trump's first term is over!";
-    } else {
+    } else if (days===1&&!inSeconds) {
+      document.querySelector('days').innerHTML='Trump';
+      document.querySelector('sometext').innerHTML='has one more day left';
+    } else if (inSeconds) {
       if (days.indexOf('.')===-1) days+='.000'
       else if (days.length-days.indexOf('.')<4) days+='0'.repeat(4-days.length+days.indexOf('.'));
       document.querySelector('days').innerHTML=days;
     }
+    else document.querySelector('days').innerHTML=days;
   };
   calc();
-  console.log(setInterval(calc,30));
-}());
+  if (inSeconds) console.log(setInterval(calc,30));
+}(window.location.pathname.includes('inseconds')));
