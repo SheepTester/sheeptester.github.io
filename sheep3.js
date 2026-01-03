dfc98eb43816337ab07ce63b2b66ecfdd01d5aa1
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-01-02 23:11:07 -0800

    Format sheep3.js and sheep3.css; drop ES5 compatibility (I inadvertently added an arrow function anyways)

--- sheep3.js
+++ sheep3.js
@@ -1,27 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(e) {
-  var homeButton = document.createElement("sheep-btn");
-  homeButton.setAttribute('tabindex', 0);
-  homeButton.setAttribute('aria-label', 'Go to SheepTester directory');
-  homeButton.setAttribute('role', 'button');
-  homeButton.addEventListener('click', function(e) {
-    document.body.classList.add('sheep-blockscreen');
-    homeButton.addEventListener('transitionend', function(e) {
-      window.requestAnimationFrame(function() {
-        // Try forcing repaint
-        homeButton.getBoundingClientRect();
-        window.location = '/?from=sheep3';
-      });
-    });
-  });
-  homeButton.addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) homeButton.click();
-  });
-  document.body.appendChild(homeButton);
-}, {once: true});
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const homeButton = document.createElement('sheep-btn')
+    homeButton.setAttribute('tabindex', 0)
+    homeButton.setAttribute('aria-label', 'Go to SheepTester directory')
+    homeButton.setAttribute('role', 'button')
+    homeButton.addEventListener('click', () => {
+      document.body.classList.add('sheep-blockscreen')
+      homeButton.addEventListener('transitionend', e => {
+        window.requestAnimationFrame(() => {
+          // Try forcing repaint
+          homeButton.getBoundingClientRect()
+          window.location = '/?from=sheep3'
+        })
+      })
+    })
+    homeButton.addEventListener('keydown', function (e) {
+      if (e.key === 'Enter') homeButton.click()
+    })
+    document.body.append(homeButton)
+  },
+  { once: true }
+)
 
 // TODO: Remove after 2023-05-29
-window.dataLayer = [['js', new Date()], ['config', 'G-9NWSPRKVS1']]
-  .map(args => function () { return arguments }(...args));
-document.head.appendChild(Object.assign(document.createElement('script'), {
-  src: '/pensive.js'
-}));
+window.dataLayer = [
+  ['js', new Date()],
+  ['config', 'G-9NWSPRKVS1']
+].map(args =>
+  (function () {
+    return arguments
+  })(...args)
+)
+document.head.append(
+  Object.assign(document.createElement('script'), {
+    src: '/pensive.js'
+  })
+)
